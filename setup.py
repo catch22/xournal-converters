f648dcfc7ec2d8631e1034f583967233b7581407
Michael Walter <michael.walter@gmail.com>
2014-12-26 14:50:59 -0800

    Improved setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,46 @@
-from setuptools import setup, find_packages
-from codecs import open
+from setuptools import setup
 from os.path import abspath, dirname, join
+import ast, io, re
 
-with open(join(abspath(dirname(__file__)), 'README.md'), encoding='utf-8') as fp:
-  long_description = fp.read()
 
+# determine __version__ from pw.py source (adapted from mitsuhiko)
+VERSION_RE = re.compile(r'__version__\s+=\s+(.*)')
+
+with io.open('xournal_converters/__init__.py', encoding='utf-8') as fp:
+  version_code = VERSION_RE.search(fp.read()).group(1)
+  version = str(ast.literal_eval(version_code))
+
+# read long description and convert to RST
+long_description = io.open(join(dirname(abspath(__file__)), 'README.md'), encoding='utf-8').read()
+try:
+  import pypandoc
+  long_description = pypandoc.convert(long_description, 'rst', format='md')
+except ImportError:
+  pass
+
+
+# invoke setuptools
 setup(
-  name='xournal-converters',
-  version='0.0.1',
-  description='Python scripts for converting Xournal documents to HTML and PDF.',
-  long_description=long_description,
-  url='https://github.com/catch22/xournal-converters',
-  author='Michael Walter',
-  author_email='michael.walter@gmail.com',
-  license='MIT',
-  classifiers=[
+  name = 'xournal-converters',
+  version = version,
+  description = 'Python scripts for converting Xournal documents to HTML and PDF.',
+  long_description = long_description,
+  url = 'https://github.com/catch22/xournal-converters',
+  author = 'Michael Walter',
+  author_email = 'michael.walter@gmail.com',
+  license = 'MIT',
+  classifiers = [
     'Development Status :: 3 - Alpha',
-    'License :: OSI Approved :: MIT License',
     'Programming Language :: Python :: 2',
+    'Environment :: Console',
+    'License :: OSI Approved :: MIT License',
   ],
-  install_requires=['reportlab'],
-  packages=find_packages(),
-  entry_points={
+  install_requires = ['reportlab'],
+  packages = ['xournal_converters'],
+  entry_points = {
     'console_scripts': [
-      'xoj2pdf=xournal_converters.pdf:main',
-      'xoj2html=xournal_converters.html:main',
+      'xoj2pdf = xournal_converters.pdf:main',
+      'xoj2html = xournal_converters.html:main',
     ],
   },
 )
