7cd4abd3c8f491168d8a7609c5b8f380d62a2bac
Michael Walter <michael.walter@gmail.com>
2017-06-06 22:29:05 +0200

    Modernize html converter.

--- xournal_converters/html.py
+++ xournal_converters/html.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
+from __future__ import print_function, unicode_literals
 from argparse import ArgumentParser
 from xml.etree.cElementTree import ElementTree
-from StringIO import StringIO
+from io import StringIO
 import gzip, cgi
 
 
@@ -24,7 +25,7 @@ def main():
 
     errors = []
 
-    io = StringIO()
+    dest = StringIO()
     for page in xml.getroot().iter('page'):
         # determine background info
         background = page.find('background')
@@ -38,63 +39,68 @@ def main():
                           background.attrib['type'])
 
         # start new page
-        print >> io, '<p>'
-        print >> io, '<svg width="%spt" height="%spt" style="border: 1px solid black; background-color: %s" viewBox="0 0 %s %s">' % (
-            page.attrib['width'], page.attrib['height'], background_color,
-            page.attrib['width'], page.attrib['height'])
+        dest.write('<p>\n')
+        dest.write(
+            '<svg width="%spt" height="%spt" style="border: 1px solid black; background-color: %s" viewBox="0 0 %s %s">\n'
+            % (page.attrib['width'], page.attrib['height'], background_color,
+               page.attrib['width'], page.attrib['height']))
         for layer in page.iter('layer'):
-            print >> io, '<g>'
+            dest.write('<g>\n')
             for item in layer:
                 if item.tag == 'stroke':
                     if item.attrib["tool"] not in ["pen", "highlighter"]:
                         errors.append(
                             "Do not know how to handle stroke tool '%s'" %
                             item.attrib['tool'])
-                    print >> io, '<path d="%s" fill="none" stroke="%s" stroke-width="%s"' % (
-                        coords(item), item.attrib["color"],
-                        item.attrib["width"]),
+                    dest.write(
+                        '<path d="%s" fill="none" stroke="%s" stroke-width="%s" '
+                        % (coords(item), item.attrib["color"],
+                           item.attrib["width"]))
                     if item.attrib["tool"] == "highlighter":
-                        print >> io, 'stroke-opacity="0.5"',
-                    print >> io, '/>'
+                        dest.write('stroke-opacity="0.5" ')
+                    dest.write('/>\n')
                 elif item.tag == 'text':
                     dy = 0
                     font_size = float(item.attrib["size"])
                     for line in item.text.split("\n"):
-                        print >> io, '<text font-family="%s" font-size="%s" x="%s" y="%s" fill="%s">%s</text>' % (
-                            item.attrib["font"], font_size, item.attrib["x"],
-                            dy + float(item.attrib["y"]) + font_size,
-                            item.attrib["color"], cgi.escape(line))
+                        dest.write(
+                            '<text font-family="%s" font-size="%s" x="%s" y="%s" fill="%s">%s</text>\n'
+                            % (item.attrib["font"], font_size,
+                               item.attrib["x"],
+                               dy + float(item.attrib["y"]) + font_size,
+                               item.attrib["color"], cgi.escape(line)))
 
                         dy += float(item.attrib["size"])
                 elif item.tag == 'image':
-                    print >> io, '<image x="%s" y="%s" width="%s" height="%s" xlink:href="data:image/png;base64,%s" />' % (
-                        item.attrib["left"], item.attrib["top"],
-                        float(item.attrib["right"]) -
-                        float(item.attrib["left"]),
-                        float(item.attrib["bottom"]) -
-                        float(item.attrib["top"]), cgi.escape(item.text))
+                    dest.write(
+                        '<image x="%s" y="%s" width="%s" height="%s" xlink:href="data:image/png;base64,%s" />\n'
+                        % (item.attrib["left"], item.attrib["top"],
+                           float(item.attrib["right"]) -
+                           float(item.attrib["left"]),
+                           float(item.attrib["bottom"]) -
+                           float(item.attrib["top"]), cgi.escape(item.text)))
                 else:
                     errors.append("Unknown item '%s'" % item.tag)
-            print >> io, '</g>'
-        print >> io, '</svg>'
-        print >> io, '</p>'
+            dest.write('</g>\n')
+        dest.write('</svg>\n')
+        dest.write('</p>\n')
 
     # render document with errors prefixed
-    print '<html>'
-    print '<body>'
+    print('<html>')
+    print('<body>')
 
     if errors:
-        print '<p>'
-        print '<ul style="color: red">'
+        print('<p>')
+        print('<ul style="color: red">')
         for e in errors:
-            print '<li>', e, '</li>'
-        print '</ul>'
-        print '</p>'
+            print('<li>', e, '</li>')
+        print('</ul>')
+        print('</p>')
 
-    print io.getvalue()
+    print(dest.getvalue())
 
-    print '</body>'
-    print '</html>'
+    print('</body>')
+    print('</html>')
 
 
 if __name__ == '__main__':
